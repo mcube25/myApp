5d99f46129f5c12be3df90f3ec79449910693298
mcube3567 <mcube3567@gmail.com>
2019-12-23 21:44:54 +0100

    new commit

--- advancedobject.js
+++ advancedobject.js
@@ -41,4 +41,75 @@ class Wizard extends player{
 }
 const wizard1 = new Wizard("shealer","healer");
 const wizard1 = new Wizard("big","hear");
+//pass by value and pass by reference
+var a =5;
+var b = 10;
+var bb =a;
+bb++ //primitive type
+//ojects
+let obj1 = {name: 'yaoo'password: '123'};
+let obj2 = obj1;
+obj2.password='easypeasy';
+console.log(obj1);
+console.log(obj2);
+//array as oject
+var c = [1,3,8,9]
+var d = c;
+d.push(8999999);
+console.log(d);
+//or to add to d array u can do
+var d = [].concat(c);
+//examlple
+let obj = {a: 'a', b: 'b', c: 'c',};
+let clone = Object.assign({}, obj);
+obj.c = 5;
+let clone2 = {...obj};
+console.log(obj);
+console.log(clone);
+console.log(clone2);
+// wat will happen if we have an object insidde an object
+let obj = {a: 'a', b: 'b', c: {deep: 'try n copy me'},};
+obj.deep = 'hahaha';
+let superClone = JSON.parse(JSON.stringify(obj);)
+console.log(superClone);
+//type coercion uses the == sign to convert tyes
+1 == '1';
+//also
+if (1){
+    console.log(5);
+}
+
+
+//exercise
+/Evaluate these:
+//#1
+[2] === [2] // false
+{} === {} // false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; //4
+const object2 = object1;  //4
+const object3 = object2; //4
+const object4 = { a: 5}; //4
+object1.a = 4;
+
+
+//#3 create two classes: an Animal class and     a Mamal class. 
+class Animal{
+constructor(name, type){
+    console.log(this);
+    this.name = name;
+    this.type = type;
+}
+}
+class Mamal{
+    constructor(name, type){
+        console.log(this);
+        this.name = name;
+        this.type = type;
+    }
+    }
+// create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+
 
