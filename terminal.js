00158ec82ee70ebb06d325964832cd95506669b5
mcube3567 <mcube3567@gmail.com>
2019-12-31 21:07:30 +0100

    terminal

---
+++ terminal.js
@@ -0,0 +1,17 @@
+//common way to access computers aroun the worldd sing d terminal
+//mac n linx 
+//ls: list everythingg on d working directory
+//pwd:   present working directory
+//cd:  means going into te folder e.g cd desktop
+//cd ..: brings u back
+//clear: to clear up the terminal
+//cd / : takes  u too d root directory
+//c ~ : takes u to user directory
+//cd Desktop/Htc one: takes  u into a folder
+//open . : opens the current folder
+//mkdir name(e.g folder name): mkdir wwebapp
+//cd webapp: goes into it
+//touch index.html : creates index.html
+//open index.html
+//open -a "sublime Text " index.html
+//rm : removes a file
